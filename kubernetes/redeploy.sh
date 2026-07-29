b6a846f67c36c1fde39af5b5e4cf84faf23694d3
Corey Merritt <coreyleemerritt@gmail.com>
2026-07-29 13:25:41 -0400

    Deprecate docker deployment; Use custom containers for postgres backup and restore

--- kubernetes/redeploy.sh
+++ kubernetes/redeploy.sh
@@ -6,21 +6,24 @@ set -euo pipefail
 script_dir="$(dirname $(readlink -f $0))"
 cd "$script_dir"
 
+# Vars
 resource_kinds=(
   "deployment"
   "statefulset"
   "daemonset"
 )
 
+# Functions
 restart_all_of_kind() {
   local kind="$1"
   kubectl get "$kind" -A -o jsonpath='{range .items[*]}{.metadata.namespace} {.metadata.name}{"\n"}{end}' \
     | while read -r namespace name; do
         echo "Restarting ${kind}/${name} in namespace ${namespace}..."
-        kubectl rollout restart "${kind}/${name}" -n "$namespace"
+        kubectl -n "$namespace" rollout restart "${kind}/${name}"
       done
 }
 
+# Entrypoint
 for kind in "${resource_kinds[@]}"; do
   restart_all_of_kind "$kind"
 done
